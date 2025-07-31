163c877549f32b306198c6f291882da756269b71
Harsha131104 <harshag197@gmail.com>
2025-07-31 08:24:53 +0530

    altered build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-# Install dependencies and run migrations
+set -o errexit  # exit on error
+
+echo "Installing dependencies..."
 pip install -r requirements.txt
+
+echo "Collecting static files..."
 python manage.py collectstatic --no-input
+
+echo "Applying database migrations..."
 python manage.py migrate
